83bcc1c93337875b76b42b4c36a11f2ec98023ea
Richard Alp <recho.alp@gmail.com>
2019-07-25 09:51:41 +0200

    Bug Fixed + New Logo

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 $(function(){
-    $(".navigation_icon").click(function(){
+    $(".navigation_icon, .navigation_ul li a").click(function(){
         $(".navigation").toggleClass('navigation-open');
     });
 });
